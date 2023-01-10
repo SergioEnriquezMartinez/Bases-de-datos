a1088bce3ecd88c1009ddd5b003144d6adf1856f
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-01-10 14:28:40 +0100

    mas fumadas

--- BD/Peliculas.sql
+++ BD/Peliculas.sql
@@ -50,3 +50,48 @@ JOIN paises pa
 ON pa.idPais = d.codPais
 WHERE pa.pais = 'España'
 ORDER BY 1 DESC
+
+/*SUBCONSULTAS
+- Se usa en los where parte derecha no izquierda
+- Tambien se pueden usar en el select pero solo tiene que devolver un resultado*/
+SELECT p.titulo
+FROM peliculas p
+WHERE p.codDirector IN (
+    SELECT d.idDirector
+    FROM directores d
+    JOIN paises pa
+    ON d.codPais = pa.idPais
+    WHERE pa.pais = 'España'
+)
+UNION
+SELECT p.titulo
+FROM peliculas p
+WHERE p.codProductor IN (
+    SELECT pr.idProductor
+    FROM productores pr
+    JOIN paises pa
+    ON d.codPais = pa.idPais
+    WHERE pa.pais = 'España'
+)
+
+/*Listado de productores y directores cuyo pais sea España*/
+
+SELECT d.nombre
+FROM directores d
+JOIN paises p
+ON d.codPais = p.idPais
+WHERE p.pais = 'España'
+UNION
+SELECT pr.nombre
+FROM productores pr
+JOIN paises p
+ON pr.codPais = p.idPais
+WHERE p.pais = 'España'
+/*Si las columnas nombre no se llamasen igual necesitariamos poner el mismo alias a ambas*/
+SELECT COALESCE (d.director, pr.productor, 'No hay') /*COALESCE: si alguno de ellos es null nos saca el siguiente*/ IFNULL (d.director, pr.productor) /*El IFNULL solo funciona en MariaDB*/
+FROM paises p
+LEFT JOIN directores d
+ON p.idPais = d.codPais
+LEFT JOIN productores pr
+ON p.idPais = pr.codPais
+WHERE p.pais = 'España'
